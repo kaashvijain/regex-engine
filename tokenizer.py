5bcafa78c4b4fdc0c57f416689573f1a29b2675f
Kaashvi <kaashvij06@gmail.com>
2025-12-25 11:12:42 +0530

    insert explicit CONCAT tokens into regex stream

---
+++ tokenizer.py
@@ -0,0 +1,49 @@
+from enum import Enum, auto
+
+class TokenType(Enum):
+    CHAR = auto()           
+    STAR = auto()           # *
+    PLUS = auto()           # +
+    QUESTION = auto()       # ?
+    OR = auto()             # |
+    LPAREN = auto()         # (
+    RPAREN = auto()         # )
+    CONCAT = auto()         # implicit concatenation
+
+class Token: 
+    def __init__(self, type_, value = None):
+        self.type = type_
+        self.value = value #only for CHAR
+
+    def __repr__(self):
+        if self.value is not None:
+            return f"{self.type.name}({self.value})"
+        return f"{self.type.name}"
+    
+def tokenize(pattern: str):
+    tokens = []
+
+    for ch in pattern:
+        if ch.isalnum():        #letters or digits
+            tokens.append(Token(TokenType.CHAR, ch))
+        elif ch == '*':
+            tokens.append(Token(TokenType.STAR))
+        elif ch == '+':
+            tokens.append(Token(TokenType.PLUS))
+        elif ch == '?':
+            tokens.append(Token(TokenType.QUESTION))
+        elif ch == '|':
+            tokens.append(Token(TokenType.OR))
+        elif ch == '(':
+            tokens.append(Token(TokenType.LPAREN))
+        elif ch == ')':
+            tokens.append(Token(TokenType.RPAREN))
+        else:
+            raise ValueError(f"Unsupported character: {ch}")
+        
+    return tokens 
+
+if __name__ == "__main__":
+    pattern = "a(b|c)*d"
+    tokens = tokenize(pattern)
+    print(tokens)
